d97c85c75118279fadac0c32f9f6ad8fb4f1c0e6
Dusan Djordjevic <dussann1997@live.com>
2021-04-08 17:35:29 +0200

    Added Cafe and User Facotry and Seeder

---
+++ database/factories/CafeFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cafe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CafeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cafe::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'city' => $this->faker->city,
+            'address' => $this->faker->address,
+            'phone' => $this->faker->numberBetween(100000,900000),
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
+}
