313ce905b2c9d4c93d79c7715e82c1155e512002
Dusan Djordjevic <dussann1997@live.com>
2021-07-02 21:10:43 +0200

    Removed name from validation

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -42,7 +42,6 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
