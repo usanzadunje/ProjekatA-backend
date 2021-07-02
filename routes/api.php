f6e692d91fbb67f8c4a4bf1bdc93ed4ef52c5cf5
Dusan Djordjevic <dussann1997@live.com>
2021-07-02 19:57:35 +0200

    Made auth routes top level

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,12 @@ use Illuminate\Support\Facades\Route;
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 
-Route::prefix('auth')->group(function() {
-    Route::get('/', AuthController::class)->middleware(['auth:sanctum']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/callback', [SocialAuthController::class, 'providerResponse']);
-    Route::post('/fcm-token', [AuthController::class, 'setFcmToken'])->middleware(['auth:sanctum']);
-    Route::post('/fcm-token/remove', [AuthController::class, 'removeFcmToken'])->middleware(['auth:sanctum']);
-});
+Route::get('/auth/user', AuthController::class)->middleware(['auth:sanctum']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/callback', [SocialAuthController::class, 'providerResponse']);
+Route::post('/fcm-token', [AuthController::class, 'setFcmToken'])->middleware(['auth:sanctum']);
+Route::post('/fcm-token/remove', [AuthController::class, 'removeFcmToken'])->middleware(['auth:sanctum']);
+
 
 // Authentication routes
 Route::prefix('users')->middleware(['auth:sanctum'])->group(function() {
@@ -55,4 +54,4 @@ Route::prefix('cafe')->group(function() {
 //Route for changing availability of tables in a certain cafe
 Route::prefix('staff')->middleware('staff')->group(function() {
 });
-    Route::post('/staff/tables/{table}/toggle', [StaffController::class, 'toggleAvailability']);
+Route::post('/staff/tables/{table}/toggle', [StaffController::class, 'toggleAvailability']);
