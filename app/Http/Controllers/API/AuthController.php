b758a623f3a3cd8f6d740d586f460de9d5a46526
Dusan Djordjevic <dussann1997@live.com>
2021-07-02 21:13:56 +0200

    Removed name from user store

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -53,7 +53,6 @@ class AuthController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
