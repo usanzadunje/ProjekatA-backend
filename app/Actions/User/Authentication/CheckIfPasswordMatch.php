566fb5eccc14d31c5eb47bcd21886bd8e6e83615
Dusan Djordjevic <dussann1997@live.com>
2021-09-15 16:03:13 +0200

    Wrong credentials now send login error key

--- app/Actions/User/Authentication/CheckIfPasswordMatch.php
+++ app/Actions/User/Authentication/CheckIfPasswordMatch.php
@@ -14,7 +14,7 @@ class CheckIfPasswordMatch
         if(!$user || !Hash::check($providedPassword, $user->password))
         {
             throw ValidationException::withMessages([
-                'password' => [
+                'login' => [
                     trans('auth.again'),
                     trans('auth.failed'),
                 ],
