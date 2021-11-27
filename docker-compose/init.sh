d86e56cd5189a1f92437b1d521ca156e066266c3
Dusan Djordjevic <dussann1997@live.com>
2021-11-27 17:03:47 +0100

    Changed production docker configuration

--- docker-compose/init.sh
+++ docker-compose/init.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 php /var/www/artisan storage:link
 php /var/www/artisan optimize
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
--- docker-compose/permissions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-chown -R root:www-data /var/www/storage
-chown -R root:www-data /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage
-chmod -R 775 /var/www/bootstrap/cache
-php /var/www/artisan optimize
